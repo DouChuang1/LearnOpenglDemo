3c20606026760186b97b4e1f6d036d959d5709d5
douch <douch@163.com>
2021-08-04 10:08:03 +0800

    绘制两个立方体模拟光源和被照亮的物体 光源模型矩阵不同于模型

--- 7-23/test.cpp
+++ 7-23/test.cpp
@@ -23,6 +23,9 @@ bool firstMouse = true;
 float lastX = 800.0f / 2.0;
 float lastY = 600.0f / 2.0;
 
+//light setting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -96,124 +99,61 @@ int main()
 	//顶点数据
 
 	float vertices[] = {
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,0.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,0.0f,
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
 
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
+		-0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
 
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
 
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
 
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
 
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,0.0f
+		-0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
 	};
 
-	glm::vec3 cubePositions[] = {
-			glm::vec3(0.0f,  0.0f,  0.0f),
-			glm::vec3(2.0f,  5.0f, -15.0f),
-			glm::vec3(-1.5f, -2.2f, -2.5f),
-			glm::vec3(-3.8f, -2.0f, -12.3f),
-			glm::vec3(2.4f, -0.4f, -3.5f),
-			glm::vec3(-1.7f,  3.0f, -7.5f),
-			glm::vec3(1.3f, -2.0f, -2.5f),
-			glm::vec3(1.5f,  2.0f, -2.5f),
-			glm::vec3(1.5f,  0.2f, -1.5f),
-			glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
 
-	unsigned int indices[] = {
-	   0, 1, 3, // first triangle
-	   1, 2, 3  // second triangle
-	};
 	VAO *vao1 = new VAO();
-
-	vector<int> layout = { 0,1 };
+	vector<int> layout = { 0 };
 	vao1->AddVertex3D(vertices, 36, layout);
-	//vao1->SetIndexData(indices,6);
 
-	Shader *shader = new Shader("vert.vs", "fragment.fs");
+	VAO *vao2 = new VAO();
+	vao2->AddVertex3D(vertices, 36, layout);
 
-	int width, height, nrChannels;
+	Shader *lightShader = new Shader("light.vs", "light.fs");
 
-	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	Shader *colorsShader = new Shader("colors.vs", "colors.fs");
 
-	GLuint texId = 0;
-	glGenTextures(1, &texId);
-	glBindTexture(GL_TEXTURE_2D, texId);
-	//纹理包装以及过滤模式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		cout << "Failed to load texture" << endl;
-	}
-	stbi_image_free(data);
-
-	GLuint texId2 = 0;
-	glGenTextures(1, &texId2);
-	glBindTexture(GL_TEXTURE_2D, texId2);
-	//纹理包装以及过滤模式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	stbi_set_flip_vertically_on_load(true);
-	data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		cout << "Failed to load texture" << endl;
-	}
-	stbi_image_free(data);
-	//glPixelStorei(GL_UNPACK_ALIGNMENT, 1);//1字节对齐
-
-	shader->use();
-	shader->setInt("ourTexture", 0);
-	shader->setInt("otherTexture", 1);
-	glViewport(0, 0, 800, 600);  //指定opengl渲染窗口位置 以及大小
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glEnable(GL_DEPTH_TEST);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  //窗口大小改变 回调执行framebuffer_size_callback 重新指定渲染视口
 	glfwSetScrollCallback(window, scroll_callback);
@@ -230,44 +170,34 @@ int main()
 		lastFrame = currentFrame;
 
 		//清屏指定颜色
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//纹理单元 0-31 编号
-		//传入纹理单元号0
-		//GLuint texLoc = glGetUniformLocation(program->programId, "tex");
-		//激活0号位单元
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texId);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texId2);
 
-		//矩阵变换
-
-		//投影矩阵
-		glm::mat4 projection;
-		projection = glm::perspective(camera->Zoom, 800.0f / 600.0f, 0.1f, 100.0f);
-
-		shader->use();
+		colorsShader->use();
+		colorsShader->setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		colorsShader->setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		//构建相机矩阵
 		glm::mat4 view;
 		view = camera->GetViewMatrix();
-		shader->setMat4("view", view);
-		shader->setMat4("projection", projection);
-		//unsigned int transformLoc = glGetUniformLocation(shader->ID, "transform");
-		//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-		//glUniform1i(texLoc, 0);
+		glm::mat4 projection = glm::perspective(glm::radians(camera->Zoom), (float)800 / (float)600, 0.1f, 100.0f);
+		colorsShader->setMat4("view", view);
+		colorsShader->setMat4("projection", projection);
+		
+		glm::mat4 model = glm::mat4(1.0f);
+		colorsShader->setMat4("model", model);
 		vao1->BindVAO();
-		for (unsigned int i = 0; i < 10; i++)
-		{
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			shader->setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		lightShader->use();
+		lightShader->setMat4("projection", projection);
+		lightShader->setMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		lightShader->setMat4("model", model);
 
+		vao2->BindVAO();
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 		processInput(window); //监听输入事件 按Escape 关闭窗口
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -276,7 +206,8 @@ int main()
 	//释放
 	glfwTerminate();
 	delete vao1;
-	delete shader;
+	delete colorsShader;
+	delete lightShader;
 	return 0;
 }
 
